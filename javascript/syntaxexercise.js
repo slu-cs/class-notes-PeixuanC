989c331f20c27871c7340089a1a68a525c3b0c19
Peixuan Cai <pcai17@stlawu.edu>
2020-01-29 13:56:54 -0500

    1

---
+++ javascript/syntaxexercise.js
@@ -0,0 +1,13 @@
+//Print the sum of the squares of the integers from 1 through 10.
+//Print the first Fibonacci number that is greater than 1000.
+//Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both.
+//Print the smallest integer that is divisible by all the integers from 1 through 10.
+let x=0;
+let z=0;
+while (x<11){
+  let y=x;
+  let z=y**2
+
+  x+=1;
+}
+console.log(z);
