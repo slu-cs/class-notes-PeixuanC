09284c79aace0ca020ff35e3ba29a5080798dd53
Peixuan Cai <pcai17@stlawu.edu>
2020-01-29 14:06:07 -0500

    3

--- javascript/syntaxexercise.js
+++ javascript/syntaxexercise.js
@@ -3,7 +3,11 @@
 //Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both.
 //Print the smallest integer that is divisible by all the integers from 1 through 10.
 let x=0;
+let z=0;
 
 while (x<11){
-console.log(x);
+  let y=x
+  let z=y**2
+x--;
 }
+console.log(z);
