ea6f6f08d54ea51a5d41753ea3fd6a7c72c84dc8
Peixuan Cai <pcai17@stlawu.edu>
2020-01-29 13:44:02 -0500

    js

---
+++ javascript/expression.js
@@ -0,0 +1,50 @@
+//expressoin in JavaScript
+
+//boolean
+true
+false
+
+//logic
+!false
+true&&false
+true||false
+
+//number(all one type)
+3.14
+
+//equality
+3===3.0
+3!==3.14
+2<3
+2<=3
+
+//Strings
+'hello'
+
+//concatenation
+'a' + 'b' ==='ab'
+
+//Arithmetic
+3+2===5
+3-2===1
+3*2===6
+3/2===1.5
+3%2===1
+3**2===9
+
+
+//type coerrcion
+'3'+2==='32'
+'3'+true==='3true'
+'3'-2===1
+
+//Loose equallity
+'3'==3
+true==1
+false==0
+false==''
+''==0
+
+
+//template Strings
+'3+2 is +${3+2}'
---
+++ javascript/statement.js
@@ -0,0 +1,27 @@
+//Statement syntax in javascript'
+
+//constant
+const pi = 3.14
+
+
+//printing
+console.log(pi);
+
+//for loop
+for( let x=0;x<3,x++){
+  console.log(x);
+}
+
+//while loop
+let x=0;
+while (x<3){
+  console.log(x);
+  x+=1;
+}
+
+//Conditional
+if(x===3){
+  console.log('x is now 3');
+}else{
+  console.log('x is now ${x}');
+}
