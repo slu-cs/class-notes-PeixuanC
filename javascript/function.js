71d25e9c0e723bd1980205cd047faffd20b79a60
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:24:22 -0500

    js function

---
+++ javascript/function.js
@@ -0,0 +1,22 @@
+//Function syntax
+
+//function definition
+const avg =function(a,b){
+  return (a+b) / 2;
+};
+
+//function call
+ console.log(avg(5,6));
+
+ //another definition
+ const print = function(a,b){
+   console.log('a=${a},b=${b}');
+ };
+
+ //undefined values
+ const result = print(7,8);
+ console.log(result);
+ print(9);
+ print();
+
+ 
