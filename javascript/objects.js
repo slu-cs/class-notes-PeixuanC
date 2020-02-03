1b599c4dd77977f4223389497bc1860986b025c8
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:15:33 -0500

    js object syntax

--- javascript/objects.js
+++ javascript/objects.js
@@ -23,4 +23,24 @@ console.log(circle.center.x);
 //undefiend subproperties
 console.log(circle.color);
 circle.color='blue';
-consle.console.log(circle);
+console.log(circle);
+
+//array
+const languages=['HTML','CSS','JS']
+console.log(languages);
+
+//accessing length and elements;
+for(let i=0;i<languages.length;i++){
+  console.log(langusges[i]);
+}
+
+//changeing elements
+languages[2]='JavaScript';
+
+//adding elements
+languages.push('MongoDB');
+
+//iterating over elements
+for(const lang of languages){
+  console.log(lang);
+}
