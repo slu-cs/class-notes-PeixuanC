65c7e959b413822b6f2925deadddcecd34348f09
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:56:12 -0500

    jsoeprob1

--- javascript/objectexercise.js
+++ javascript/objectexercise.js
@@ -12,7 +12,7 @@ const Course=function(subject,number){
 
 const cs364=new Course("CS",364);
 const cs332=new Course("CS",332);
-const cs250=new Course("CS",364250);
+const cs250=new Course("CS",250);
 const ma280=new Course("MATH",280);
 const ma230=new Course("MATH",230);
 const sta113=new Course("STAT",113);
@@ -31,3 +31,14 @@ sp20.push(sta113);
 for(let i=0;i<sp20.length;i++){
   console.log(sp20[i]);
 };
+
+sp20.prototype.max=function(){
+  let ret=0;
+  for(let i=0;i<sp20.length;1++){
+    if(sp20[i].number>ret){
+      let ret=sp20[i].number;
+    }
+
+  }return ret;
+
+}console.log(sp20.max());
