4ab99d32bb2e95b8c0c26ea781192e38f334aee4
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:49:56 -0500

    object exercise prob1

---
+++ javascript/objectexercise.js
@@ -0,0 +1,33 @@
+
+
+//1. Create an array of objects representing the courses
+// you are taking this semester. Each course should have
+// a subject (e.g. 'CS') and a number (e.g. 332). Iterate through
+//this array to find the largest course number in your schedule.
+const Course=function(subject,number){
+  this.subject=subject;
+  this.number=number;
+
+};
+
+const cs364=new Course("CS",364);
+const cs332=new Course("CS",332);
+const cs250=new Course("CS",364250);
+const ma280=new Course("MATH",280);
+const ma230=new Course("MATH",230);
+const sta113=new Course("STAT",113);
+
+
+
+const sp20=[];
+
+sp20.push(cs364);
+sp20.push(cs332);
+sp20.push(cs250);
+sp20.push(ma280);
+sp20.push(ma230);
+sp20.push(sta113);
+
+for(let i=0;i<sp20.length;i++){
+  console.log(sp20[i]);
+};
