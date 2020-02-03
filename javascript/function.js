e67ee8f014fc7bf2e5c400cb50c3fdda175dd88a
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:34:15 -0500

    js method

--- javascript/function.js
+++ javascript/function.js
@@ -10,7 +10,7 @@ const avg =function(a,b){
 
  //another definition
  const print = function(a,b){
-   console.log('a=${a},b=${b}');
+   console.log(a=${a},b=${b});
  };
 
  //undefined values
@@ -19,4 +19,33 @@ const avg =function(a,b){
  print(9);
  print();
 
- 
+ //object litersl with a method
+ const rectangle={
+   width:10,
+   height:20,
+   area:function(){
+     return this.wodth*this.height;
+   }
+ };
+
+ //method call
+ console.log(rectangle.area());
+
+ //conatructor definition
+ const Rectangle = function(width, height){
+   this.width=width;
+   this.height=height;
+ };
+
+ //Construction objects
+const small=new Rectangle(1,2);
+const large=new Rectangle(10,20);
+
+//shared method for all rectangles
+//Rectangle.prototype.area=function(){
+//  return this.width*this.height;
+//};
+
+
+console.log(small.area());
+console.log(large.area());
