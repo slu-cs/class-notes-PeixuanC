1fa0b66bb7f0b9dda8bce4eb6b3c3f31f3b8f452
Peixuan Cai <pcai17@stlawu.edu>
2020-02-03 13:07:27 -0500

    js object syntax

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,3 +3,24 @@
 //Object literal
 const point ={x:1 ,y:2};
 console.log(point);
+
+
+//Accessing porperties
+consle.log(point.x);
+
+//changing porperties
+point.x=3
+
+//nested object literal
+const circle={
+  center:point'
+  radious:4
+};
+
+//accessing subproperties
+console.log(circle.center.x);
+
+//undefiend subproperties
+console.log(circle.color);
+circle.color='blue';
+consle.console.log(circle);
