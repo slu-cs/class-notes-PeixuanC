338c45cd97f8338636ee621d3414ae2496d90e1e
Peixuan Cai <pcai17@stlawu.edu>
2020-02-05 14:14:00 -0500

    2.5e

--- javascript/jscallbackexercise.js
+++ javascript/jscallbackexercise.js
@@ -1,7 +1,11 @@
 //1. JavaScript arrays have a filter method that works like this:
 
-const odds = numbers.filter(x => x%2 > 0);
+
 //Try it out, and then write your own filter function, which would be called like this instead:
 
 const numbers = [1, 2, 3, 4, 5];
 //const odds = filter(numbers, x => x%2 > 0);
+
+const odds = numbers.filter(x => x%2 > 0);
+
+console.log(odds);
