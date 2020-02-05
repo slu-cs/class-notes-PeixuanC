9b8f40cab7afe329ac04a5215000caafde22248b
Peixuan Cai <pcai17@stlawu.edu>
2020-02-05 13:59:42 -0500

    jsinput

--- javascript/input.js
+++ javascript/input.js
@@ -1,6 +1,7 @@
 //syntax for console file input
 
 const readline =require('readline');
+const fs=require('fs');
 
 //console configuration
 const user =readline.createInterface({
@@ -12,7 +13,24 @@ const user =readline.createInterface({
 user.question('Filename:',function(filename){
   console.log(filename);
 
+  const file=realine.createInterface({
+    input: fs.createReadStream(filename)
+  });
+
+  //Asynchronous line-by lione inpout
+  file.on('line',function(line){
+    console.log(line);
+  });
+
+  //end the program when the file closes
+  file.on('close', function(){
+    process.exit(0);
+  })
+
 });
 
 //this part isnt after the console
 console.log('here');
+
+
+//fie configuration
